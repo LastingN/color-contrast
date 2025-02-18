3316ce086742485f7828d26b6c6e99d6f95fc5f9
Burnfete <colorcontrastcheck@163.com>
2025-02-18 10:56:07 +0800

    试试3

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,12 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
-  // base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/color-contrast/' : '/', // 修改为相对路径
+  build: {
+    outDir: 'dist', // 输出目录
+    assetsDir: 'assets', // 静态资源目录
+    emptyOutDir: true, // 构建前清空输出目录
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
